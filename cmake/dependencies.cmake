d059d107270c2451c2ae3b1ceaf5bdc7a6c2c6f1
GK72 <gk.project72@gmail.com>
2024-03-16 18:51:01 +0100

    Fix(deps): Linking GoogleTest CMake target

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,5 +1,12 @@
-include(cmake/cpm.cmake)
+include("${CMAKE_CURRENT_LIST_DIR}/cpm.cmake")
 
-CPMAddPackage("gh:google/googletest#v1.14.0")
 CPMAddPackage("gh:fmtlib/fmt#10.2.1")
 CPMAddPackage("gh:gabime/spdlog#v1.13.0")
+CPMAddPackage("gh:google/googletest#v1.14.0")
+
+CPMAddPackage(
+    NAME benchmark
+    GITHUB_REPOSITORY google/benchmark
+    VERSION 1.8.3
+    OPTIONS "BENCHMARK_ENABLE_TESTING OFF"
+)
