01271e8bf12e3fac27ab9fafff5f4755763b2efa
GK72 <gk.project72@gmail.com>
2024-11-19 18:38:36 +0100

    Refact: fleshing out `expected`
    Switching from `std::variant` to `union`

--- include/nova/expected.hh
+++ include/nova/expected.hh
@@ -15,6 +15,18 @@
 
 namespace nova {
 
+namespace detail {
+
+    // Tags for choosing the member in the `union`.
+
+    struct expect_t   { explicit expect_t() = default;   };
+    struct unexpect_t { explicit unexpect_t() = default; };
+
+    inline constexpr expect_t expect;
+    inline constexpr unexpect_t unexpect;
+
+} // namespace detail
+
 struct empty {};
 
 template <typename E>
@@ -30,49 +42,53 @@ public:
     using error_type      = E;
     using unexpected_type = unexpected<E>;
 
-    template <typename U> constexpr expected(const U& value)
-        : _value_or_unexpected(value)
+    constexpr expected(const value_type& value)
+        : m_vex(detail::expect, value)
+    {}
+
+    constexpr expected(value_type&& value)
+        : m_vex(detail::expect, std::move(value))
     {}
 
     constexpr expected(const unexpected_type& unexpected)
-        : _value_or_unexpected(unexpected.value)
+        : m_vex(detail::unexpect, unexpected.value)
     {}
 
-    constexpr expected(unexpected_type&& unexpected)
-        : _value_or_unexpected(std::move(unexpected.value))
-    {}
+    // constexpr expected(unexpected_type&& unexpected)
+        // : _value_or_unexpected(std::move(unexpected.value))
+    // {}
 
     template <typename E2>
         requires std::is_convertible_v<typename E2::type, E>
     constexpr expected(const E2& unexpected)
-        : _value_or_unexpected(unexpected.value)
+        : m_vex(detail::unexpect, unexpected.value)
     {}
 
-    template <typename E2>
-        requires std::is_convertible_v<typename E2::type, E>
-    constexpr expected(E2&& unexpected)
-        : _value_or_unexpected(std::move(unexpected.value))
-    {}
+    // template <typename E2>
+        // requires std::is_convertible_v<typename E2::type, E>
+    // constexpr expected(E2&& unexpected)
+        // : _value_or_unexpected(std::move(unexpected.value))
+    // {}
 
-    [[nodiscard]] constexpr const T* operator->() const noexcept    { return &value(); }
-    [[nodiscard]] constexpr T* operator->() noexcept                { return &value(); }
-    [[nodiscard]] constexpr const T& operator*() const& noexcept    { return value(); }
-    [[nodiscard]] constexpr T& operator*() & noexcept               { return value(); }
-    [[nodiscard]] constexpr const T&& operator*() const&& noexcept  { return value(); }
-    [[nodiscard]] constexpr T&& operator*() && noexcept             { return value(); }
+    [[nodiscard]] constexpr const T*  operator->() const   noexcept { return &m_vex.impl.v; }
+    // [[nodiscard]] constexpr       T*  operator->()         noexcept { return &m_vex.impl.v; }
+    [[nodiscard]] constexpr const T&  operator*()  const&  noexcept { return m_vex.impl.v; }
+    // [[nodiscard]] constexpr       T&  operator*()       &  noexcept { return m_vex.impl.v; }
+    // [[nodiscard]] constexpr const T&& operator*()  const&& noexcept { return m_vex.impl.v; }
+    // [[nodiscard]] constexpr       T&& operator*()       && noexcept { return m_vex.impl.v; }
 
-    [[nodiscard]] constexpr bool has_value() const noexcept         { return std::holds_alternative<T>(_value_or_unexpected); }
+    [[nodiscard]] constexpr bool has_value() const noexcept         { return m_vex.value; }
     [[nodiscard]] constexpr explicit operator bool() const noexcept { return has_value(); }
 
-    [[nodiscard]] constexpr const T& value() const&                 { return std::get<T>(_value_or_unexpected); }
-    [[nodiscard]] constexpr T& value() &                            { return std::get<T>(_value_or_unexpected); }
+    [[nodiscard]] constexpr const T& value() const&                 { return m_vex.impl.v; }
+    // [[nodiscard]] constexpr T& value() &                            { return std::get<T>(_value_or_unexpected); }
     // [[nodiscard]] constexpr const T&& value() const&&               { return std::get<T>(_value_or_unexpected); }
     // [[nodiscard]] constexpr T&& value() &&                          { return std::get<T>(_value_or_unexpected); }
 
-    [[nodiscard]] constexpr const E& error() const&                 { return std::get<E>(_value_or_unexpected); }
-    [[nodiscard]] constexpr E& error() &                            { return std::get<E>(_value_or_unexpected); }
-    [[nodiscard]] constexpr const E&& error() const&&               { return std::get<E>(_value_or_unexpected); }
-    [[nodiscard]] constexpr E&& error() &&                          { return std::get<E>(_value_or_unexpected); }
+    [[nodiscard]] constexpr const E& error() const&                 { return m_vex.impl.e; }
+    // [[nodiscard]] constexpr E& error() &                            { return std::get<E>(_value_or_unexpected); }
+    // [[nodiscard]] constexpr const E&& error() const&&               { return std::get<E>(_value_or_unexpected); }
+    // [[nodiscard]] constexpr E&& error() &&                          { return std::get<E>(_value_or_unexpected); }
 
     template <typename U>
     [[nodiscard]] constexpr
@@ -80,14 +96,77 @@ public:
         return has_value() ? value() : static_cast<T>(std::forward<U>(def));
     }
 
-    template <typename U>
-    [[nodiscard]] constexpr
-    T value_or(U&& def) && {
-        return has_value() ? value() : static_cast<T>(std::forward<U>(def));
-    }
+    // TODO: test ?
+    // template <typename U>
+    // [[nodiscard]] constexpr
+    // T value_or(U&& def) && {
+        // return has_value() ? value() : static_cast<T>(std::forward<U>(def));
+    // }
 
 private:
-    std::variant<T, E> _value_or_unexpected;
+    union vex_impl {
+        template <typename ...Args>
+        constexpr vex_impl(detail::expect_t, Args&& ...args)
+            : v(std::forward<Args>(args)...)
+        {}
+
+        template <typename ...Args>
+        constexpr vex_impl(detail::unexpect_t, Args&& ...args)
+            : e(std::forward<Args>(args)...)
+        {}
+
+        // TODO(feat): Copy and move constructors
+        //               default if trivially copyable/movable
+        //               else delete
+
+        constexpr ~vex_impl() requires std::is_trivially_destructible_v<T>
+                                    && std::is_trivially_destructible_v<E>
+                              = default;
+
+        /**
+         * @brief   Outer class, `vex` knows how to destruct.
+         *
+         * Information about which member is active is carried outside this union.
+         */
+        constexpr ~vex_impl() { /* `vex` destructs */ }
+
+        T v;
+        E e;
+    };
+
+    /**
+    * @brief    Value or unEXpected.
+    *
+    * Contains the `union` in a (somewhat) safe manner.
+    */
+    struct vex {
+        template <typename ...Args>
+        constexpr vex(detail::expect_t tag, Args&& ...args)
+            : impl(tag, std::forward<Args>(args)...)
+            , value(true)
+        {}
+
+        template <typename ...Args>
+        constexpr vex(detail::unexpect_t tag, Args&& ...args)
+            : impl(tag, std::forward<Args>(args)...)
+            , value(false)
+        {}
+
+        constexpr ~vex() requires std::is_trivially_destructible_v<T>
+                               && std::is_trivially_destructible_v<E>
+                         = default;
+
+        constexpr ~vex() {
+            if (value) {
+                std::destroy_at(std::addressof(impl.v));
+            } else {
+                std::destroy_at(std::addressof(impl.e));
+            }
+        }
+
+        vex_impl impl;
+        bool value;
+    } m_vex;
 
 };
 
--- unit-tests/expected.cc
+++ unit-tests/expected.cc
@@ -2,6 +2,7 @@
 
 #include <gtest/gtest.h>
 
+#include <memory>
 #include <string_view>
 
 TEST(Expected, TrivialTypes) {
@@ -25,7 +26,7 @@ TEST(Expected, TrivialTypes) {
 
         using namespace std::literals::string_view_literals;
 
-        EXPECT_TRUE(!ret);
+        EXPECT_TRUE(not ret);
         EXPECT_EQ(ret.error(), "Error message"sv);
     }
 }
@@ -38,4 +39,64 @@ TEST(Expected, NonTrivialTypes) {
 
     const auto x = nova::expected<S, int>(S{ });
     EXPECT_TRUE(x);
+}
+
+TEST(Expected, NonTrivialDestructor_LeakSanyiOn) {
+    const auto x = nova::expected<std::unique_ptr<int>, int>(std::make_unique<int>(9));
+    EXPECT_TRUE(x);
+    // Leak sanitizer fires if the destructor of the implementation (`vex`) is empty.
+}
+
+TEST(Expected, SameTypes) {
+    constexpr auto x = nova::expected<int, int>(9);
+    ASSERT_TRUE(x);
+    EXPECT_EQ(*x, 9);
+
+    constexpr auto y = nova::expected<int, int>(nova::unexpected{ 8 });
+    ASSERT_TRUE(not y);
+    EXPECT_EQ(y.error(), 8);
+}
+
+TEST(Expected, ValueConversion) {
+    constexpr auto x = nova::expected<std::string_view, int>("hello");
+    EXPECT_EQ(x.value(), "hello");
+}
+
+TEST(Expected, ErrorConversion) {
+    constexpr auto x = nova::expected<int, std::string_view>(nova::unexpected{ "hello" });
+    EXPECT_EQ(x.error(), "hello");
+}
+
+TEST(Expected, Deref) {
+    constexpr auto x = nova::expected<int, std::string_view>(9);
+    EXPECT_EQ(*x, 9);
+}
+
+TEST(Expected, HasValue) {
+    constexpr auto x = nova::expected<int, std::string_view>(1);
+    EXPECT_TRUE(x.has_value());
+
+    constexpr auto y = nova::expected<int, std::string_view>(nova::unexpected{ "a" });
+    EXPECT_TRUE(not y.has_value());
+}
+
+TEST(Expected, BoolConversion) {
+    constexpr auto x = nova::expected<int, std::string_view>(1);
+    EXPECT_TRUE(x);
+
+    constexpr auto y = nova::expected<int, std::string_view>(nova::unexpected{ "a" });
+    EXPECT_TRUE(not y);
+}
+
+TEST(Expected_MondadicOps, ValueOr) {
+    constexpr auto x = nova::expected<int, std::string_view>(9);
+    EXPECT_EQ(x.value_or(2), 9);
+
+    constexpr auto y = nova::expected<int, std::string_view>(nova::unexpected{ "a" });
+    EXPECT_EQ(y.value_or(2), 2);
+}
+
+TEST(Expected_MondadicOps, ValueOr_OnRValue) {
+    EXPECT_EQ(( nova::expected<int, std::string_view>(9).value_or(2) ), 9);
+    EXPECT_EQ(( nova::expected<int, std::string_view>(nova::unexpected{ "a" }).value_or(2) ), 2);
 }
