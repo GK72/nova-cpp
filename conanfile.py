15b315ada2e58e3da5178d9e32658fb16c35eaea
GK72 <gk.project72@gmail.com>
2025-01-04 14:52:20 +0100

    version bump

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.build import check_min_cppstd
 
 class Nova(ConanFile):
     name = "nova"
-    version = "0.1.3"
+    version = "0.1.4"
     package_type = "library"
 
     license = "BSL"
