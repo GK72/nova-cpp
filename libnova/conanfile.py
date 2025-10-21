7a92617cc489c6c86b32d0fbbef46fb0d399aafb
GK72 <gk.project72@gmail.com>
2025-10-21 11:34:53 +0200

    Misc: Added `-Wno-error=null-dereference
    Most (if not all) of these errors are due to compiler bugs.

--- libnova/conanfile.py
+++ libnova/conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.build import check_min_cppstd
 
 class Nova(ConanFile):
     name = "nova"
-    version = "0.8.0"
+    version = "0.8.1"
     package_type = "library"
 
     license = "BSL"
