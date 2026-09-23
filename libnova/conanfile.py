73a7aa56efc79087225b1408a7e20054d6535874
GK72 <gk.project72@gmail.com>
2026-09-23 19:03:38 +0200

    Fix: Including CMake modules in Conan package
    - Added failure on error and missing variables to `run` script.
    - Removed unncessary `CMakeUserPresets.json`.

--- libnova/conanfile.py
+++ libnova/conanfile.py
@@ -1,3 +1,5 @@
+import os
+
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 from conan.tools.build import check_min_cppstd
@@ -64,3 +66,7 @@ class Nova(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = ["nova"]
+
+        module_dir = os.path.join("lib", "cmake", "nova")
+        self.cpp_info.builddirs = [module_dir]
+        self.cpp_info.set_property("cmake_build_modules", [os.path.join(module_dir, "novaModulePath.cmake")])
